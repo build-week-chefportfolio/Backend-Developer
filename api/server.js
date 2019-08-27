27d5f62576c3c1891a2b0289435596a83fedfd9d
bwpeck <benjaminwade483@yahoo.com>
2019-08-27 17:21:31 -0400

    cors removed

--- api/server.js
+++ api/server.js
@@ -12,7 +12,7 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+//server.use(cors());
 
 server.use("/api/auth", authRouter);
 server.use("/api/posts", postsRouter);
