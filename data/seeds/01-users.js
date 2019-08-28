c9c88cb28959ca9069ee7ce40cdcc4ab07e54a1f
bwpeck <benjaminwade483@yahoo.com>
2019-08-27 23:21:07 -0400

    try this

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -5,7 +5,8 @@ exports.seed = function (knex) {
       return knex('users').insert([
         { id: 1, username: 'Carl', password: 'mutumbo' },
         { id: 2, username: 'Sascha', password: 'howcanidothis'},
-        { id: 3, username: 'Lukas', password: '12345' }
+        { id: 3, username: 'Lukas', password: '12345' },
+        { id: 4, username: "charles", password: "michael" }
       ]);
     });
 };
